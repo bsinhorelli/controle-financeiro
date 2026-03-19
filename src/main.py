3872e3ccc0811282f821f279771fd1993eeb4066
Bruno Sinhorelli <b.sinhorelli@gmail.com>
2026-03-19 19:59:33 -0300

    Criação do menu visual

---
+++ src/main.py
@@ -0,0 +1,23 @@
+def add_receita():
+
+
+print('='*40)
+print(f'{'BEM-VINDO AO SEU CONTROLE FINANCEIRO!':^40}')
+print('='*40)
+
+receitas = []
+despesas = []
+
+while True:
+    print('1 - Adicionar receita\n'
+          '2 - Adicionar despesa\n'
+          '3 - Listar transações\n'
+          '4 - Mostrar saldo atual')
+
+    opcao = int(input('Escolha uma opção: '))
+
+    if opcao == 1:
+
+
+    break
+
