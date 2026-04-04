543203a7c73c4c334739e848a35f4b28e489202b
Bruno Sinhorelli <b.sinhorelli@gmail.com>
2026-04-04 15:33:39 -0300

    feat: implementar cadastro de receitas com validação e armazenamento em lista

--- src/main.py
+++ src/main.py
@@ -14,19 +14,53 @@ def ler_opcao():
                         print('Digite apenas números!')
       
       return opcao
+
+def adicionar_receita():
+      receitas = dict()
+      receitas['tipo'] = 'Receita'
+      
+      descricao = input('Descrição da Receita: ')
+      receitas['Descrição'] = descricao
+      
+      while True:
+            try:
+                  valor = float(input('Digite o valor da receita: R$ '))
+                  if valor > 0:
+                        break
+                  else:
+                        print('Digite um valor positivo!')
+            except ValueError:
+                  print('Digite um valor Real, Apenas Números!')
+      receitas['Valor (R$)'] = valor
+      
+      return receitas
+      
+      
+      
+      
+      
+      
+      
                   
 
 def main():
+      transacoes = []
+      
       while True:
             mostrar_menu()
             opcao = ler_opcao()
             
             if opcao == 1:
-                  print('Adicionar Receita')
+                  receitas = adicionar_receita()
+                  transacoes.append(receitas)
+                  print(receitas)
+                  
+                  
             elif opcao == 2:
                   print('Adicionar Despesa')
             elif opcao == 3:
                   print('Ver Resumo Financeiro')
+                  print(transacoes)
             else:
                   print('encerrando programa...')
                   break
